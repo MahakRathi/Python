5110c10a4b93ca5c81baa3033442221ee0951ca8
Mahak Rathi <136226699+MahakRathi@users.noreply.github.com>
2025-07-05 16:04:14 +0000

    Updated temperature conversion logic

--- Day 1/1.1.py
+++ Day 1/1.1.py
@@ -1,5 +1,5 @@
-'''Write a Python program that takes temperature in Fahrenheit as input and converts it to Celsius '''
+#Write a Python program that takes temperature in Fahrenheit as input and converts it to Celsius 
 Fahrenheit_String=input('enter a no. in fahrenheit: ');
-Fahrenheit=float(Fahrenheit_String);67
-Celcius = ((Fahrenheit - 32) * 5)/967;
+Fahrenheit=float(Fahrenheit_String);
+Celcius = ((Fahrenheit - 32) * 5)/9;
 print("Farenheit converted into celcius" ,Celcius); 
---
+++ Day 1/1.2.py
@@ -0,0 +1,1 @@
+#Write a Python program that checks if a given integer is even or odd.
