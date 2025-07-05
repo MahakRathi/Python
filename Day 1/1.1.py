18bb974189836b5e98850b5a1e5ae9cc4c1b0782
Mahak Rathi <136226699+MahakRathi@users.noreply.github.com>
2025-07-05 15:58:53 +0000

    Initial commit from Codespace

---
+++ Day 1/1.1.py
@@ -0,0 +1,5 @@
+'''Write a Python program that takes temperature in Fahrenheit as input and converts it to Celsius '''
+Fahrenheit_String=input('enter a no. in fahrenheit: ');
+Fahrenheit=float(Fahrenheit_String);67
+Celcius = ((Fahrenheit - 32) * 5)/967;
+print("Farenheit converted into celcius" ,Celcius); 
