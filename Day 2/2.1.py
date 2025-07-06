de9bd26b4b893436610d07a6ae2afdfd0dd88671
Mahak Rathi <136226699+MahakRathi@users.noreply.github.com>
2025-07-06 18:06:03 +0000

    First Commit

---
+++ Day 2/2.1.py
@@ -0,0 +1,5 @@
+#Ask the user for their name and age, then display a greeting showing how old they'll be next year.
+name=input("Enter your Name: ");
+age=int(input("Enter your Age: "))
+
+print("Hello! ",name, " You will be ",age+1," Years old next year")
