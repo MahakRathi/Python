656ee7c54945ad567c210fa56109dc8d244cdba2
Mahak Rathi <136226699+MahakRathi@users.noreply.github.com>
2025-07-07 06:17:37 +0000

    First commit

---
+++ Day 3/3.1.py
@@ -0,0 +1,6 @@
+# Reverse a String
+# Write a program to reverse a string entered by the user.
+
+Value=input("Enter a String: ");
+reverse_value=Value[::-1];
+print("Reverse String  ", reverse_value);
